4661660ae4d8d71c817f7a85fba766f2e55192bd
Charles Swaleh <charlswall@live.com>
2023-04-23 14:14:46 +0300

    Created the GET /students/highest-grade route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,4 +9,10 @@ class StudentsController < ApplicationController
     sorted_by_grades = students.sort { |a, b| b.grade <=> a.grade }
     render json: sorted_by_grades
   end
+
+  def highest_grade
+    max_grade = Student.all.map { |student| student.grade }.max
+    student_with_highest_grade = Student.find_by(grade: max_grade)
+    render json: student_with_highest_grade
+  end
 end
